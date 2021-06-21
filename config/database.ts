7cf5f2ad10ba370cd17fc8db31ae521ee27df0d8
brendamatias <brendamatias.sobral@gmail.com>
2021-06-21 20:05:44 -0300

    config database

--- config/database.ts
+++ config/database.ts
@@ -1,6 +1,30 @@
+import Url from 'url-parse';
+
 import Env from '@ioc:Adonis/Core/Env';
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database';
 import { OrmConfig } from '@ioc:Adonis/Lucid/Orm';
+
+let DB_DATABASE_URL = '';
+
+const dbConfig = {
+  host: Env.get('PG_HOST'),
+  port: Env.get('PG_PORT'),
+  user: Env.get('PG_USER'),
+  password: Env.get('PG_PASSWORD', ''),
+  database: Env.get('PG_DB_NAME'),
+};
+
+if (Env.get('DATABASE_URL')) {
+  DB_DATABASE_URL = new Url(Env.get('DATABASE_URL'));
+
+  if (DB_DATABASE_URL) {
+    dbConfig.host = DB_DATABASE_URL.host;
+    dbConfig.port = Number('');
+    dbConfig.user = DB_DATABASE_URL.username;
+    dbConfig.password = DB_DATABASE_URL.password;
+    dbConfig.database = DB_DATABASE_URL.pathname.substr(1);
+  }
+}
 
 const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
   connection: Env.get('DB_CONNECTION'),
@@ -9,11 +33,11 @@ const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
     pg: {
       client: 'pg',
       connection: {
-        host: Env.get('PG_HOST'),
-        port: Env.get('PG_PORT'),
-        user: Env.get('PG_USER'),
-        password: Env.get('PG_PASSWORD', ''),
-        database: Env.get('PG_DB_NAME'),
+        host: dbConfig.host,
+        port: dbConfig.port,
+        user: dbConfig.user,
+        password: dbConfig.password,
+        database: dbConfig.database,
       },
       healthCheck: false,
       debug: false,
