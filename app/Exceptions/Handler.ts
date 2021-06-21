ccb75cf4682b131febb9845e56b9f5fd68ca81c5
brendamatias <brendamatias.sobral@gmail.com>
2021-06-21 15:01:58 -0300

    config exceptions

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -1,23 +1,22 @@
-/*
-|--------------------------------------------------------------------------
-| Http Exception Handler
-|--------------------------------------------------------------------------
-|
-| AdonisJs will forward all exceptions occurred during an HTTP request to
-| the following class. You can learn more about exception handling by
-| reading docs.
-|
-| The exception handler extends a base `HttpExceptionHandler` which is not
-| mandatory, however it can do lot of heavy lifting to handle the errors
-| properly.
-|
-*/
-
-import Logger from '@ioc:Adonis/Core/Logger'
-import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler';
+import Logger from '@ioc:Adonis/Core/Logger';
 
 export default class ExceptionHandler extends HttpExceptionHandler {
-  constructor () {
-    super(Logger)
+  constructor() {
+    super(Logger);
+  }
+
+  async handle(error, { response }) {
+    if (error.name === 'CustomException') {
+      return response
+        .status(error.status)
+        .send({ error: { message: error.message } });
+    }
+
+    return response.status(error.status || 500).send({
+      error: {
+        message: 'Estamos com problema agora, tente novamente mais tarde',
+      },
+    });
   }
 }
